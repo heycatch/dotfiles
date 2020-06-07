a337c99135b8a008acff366581a1478c733f71bb
vexilology <andreyisback@yandex.ru>
2020-06-08 01:14:16 +0400

    customize bash

--- apt/install.sh
+++ apt/install.sh
@@ -5,16 +5,11 @@ file=programs
 exec 9<$file
 
 while read -u 9 line; do
-  echo "~~~~~~~~~~~~~~~~~"
   echo -e "Installing \e[42m$line\e[0m"
-  echo "~~~~~~~~~~~~~~~~~"
   sudo apt install $line
-  echo "-----------------"
-  echo -e "\e[31mPress enter for next...\e[0m"
-  echo "-----------------"
+  echo -e "\e[3mPress enter for next...\e[0m"
   read
 done
 
 exec 9<&-
-
-echo -e "\e[3mDone.\e[0m"
+echo "*** Completed ***"
