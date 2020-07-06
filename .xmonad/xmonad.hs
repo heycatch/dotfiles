8cc558577504fa76e0ccba3b2ca9a091d35a8310
vexilology <andreyisback@yandex.ru>
2020-07-06 21:45:34 +0400

    xmonad settings

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,16 @@
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.CustomKeys
+import XMonad.Util.EZConfig
+
+main = do
+xmproc <- spawnPipe "xmobar"
+xmonad $ defaultConfig
+  { terminal = "xterm"
+  , manageHook = manageDocks <+> manageHook defaultConfig
+  , layoutHook = avoidStruts $ layoutHook defaultConfig
+  , logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn xmproc, ppOrder = \(ws:_:t:_) -> [ws,t] }
+  , borderWidth = 3
+  }
