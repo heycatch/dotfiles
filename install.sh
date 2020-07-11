b2318f2e74b3003e3e826c7594a90088f98e2357
vexilology <andreyisback@yandex.ru>
2020-07-11 21:35:32 +0400

    dmenu

--- install.sh
+++ install.sh
@@ -6,27 +6,30 @@ if [ $(which apt) ]; then
   sudo apt install git
   echo "Ok..."
 else
-  echo "Installing for only apt packages."
+  echo "Installing for only apt packages"
 fi
 
 
 echo "***************************************"
-echo "Cloning Vundle."
+echo "Cloning Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "Completed."
-echo "Launch vim and run :PluginInstall or :h vundle for docs."
+echo "Ok..."
+echo "Launch vim and run :PluginInstall or :h vundle for docs"
 
 
 echo "***************************************"
-echo -n "Installing xmonad? (y/n) "
+echo -n "Installing xmonad + dmenu? (y/n) "
 read item
 case "$item" in
   y|Y) echo "Installing..."
     sudo apt install xmonad
+    sudo apt install dmenu
+    sudo sed -i s/dmenu/"dmenu -b"/2 /usr/bin/dmenu_run
+    echo "Ok..."
     ;;
-  n|N) echo "Exit."
+  n|N) echo "Exit"
     exit 0
     ;;
-  *) echo "Default == no, exit now."
+  *) echo "Default == no, exit now"
     ;;
 esac
