70e94f40522955b5f6a375fac64e885f1b433271
vexilology <andreyisback@yandex.ru>
2020-11-17 19:12:58 +0400

    required open-source programs for windows

---
+++ for_windows.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import webbrowser as wb
+
+
+wb.open_new_tab('https://github.com/FarGroup/FarManager')
+wb.open_new_tab('https://github.com/processhacker/processhacker')
+wb.open_new_tab('https://github.com/wireshark/wireshark')
+wb.open_new_tab('https://github.com/pbatard/rufus')
+wb.open_new_tab('https://github.com/qbittorrent/qBittorrent')
+wb.open_new_tab('https://github.com/keepassx/keepassx')
+wb.open_new_tab('https://github.com/Eloston/ungoogled-chromium')
+wb.open_new_tab('https://github.com/sumatrapdfreader/sumatrapdf')
+wb.open_new_tab('https://github.com/videolan/vlc')
+wb.open_new_tab('https://github.com/GNOME/gimp')
+wb.open_new_tab('https://sourceforge.net/projects/jpegview/')
+wb.open_new_tab('https://sourceforge.net/projects/vcxsrv/')
+wb.open_new_tab('https://www.cpuid.com/softwares/cpu-z.html')
